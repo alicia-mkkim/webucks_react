6bba86549d90c769c5fa3a39aba171d8aa20a463
alicia-mkkim <alicia.mkkim@gmail.com>
2022-02-28 21:30:27 +0900

    Add : Mission 0B - sass 적용 완료

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Detail.css'
-import '../../styles/common.css'
+import './Detail.scss'
+import styles from '../../styles/common.module.scss'
 
 function Detail() {
     return (
         <>
-            <div className="container">
-                <header>
-                    <h1 className="logo">WeBucks</h1>
+            <div className={styles.container}>
+                <header className={styles.header}>
+                    <h1 className={styles.logo}>WeBucks</h1>
                     <nav>
-                        <ul className="menu">
+                        <ul className={styles.menu}>
                             <li>COFFEE</li>
                             <li>MENU</li>
                             <li>STORE</li>
@@ -17,95 +17,93 @@ function Detail() {
                         </ul>
                     </nav>
                 </header>
-                <main>
-                    <section className="sub-title">
-                        <h2>콜드 브루</h2>
-                        <ul>
-                            <li>홈</li>
-                            <li>음료</li>
-                            <li>에스프레소</li>
-                            <li>화이트 초콜릿 모카</li>
-                        </ul>
-                    </section>
-                    <section className="contents">
-                        <article className="imageBox">
-                            <img src="/images/coffeeImg1.jpeg" alt="coffee" />
-                        </article>
-                        <aside className="details">
-                            <div className="coffee-title">
-                                <h2>화이트 초콜릿 모카</h2>
-                                <p>White Chocolate Mocha</p>
-                                <button className="heart">🤍</button>
+                <section className="sub-title">
+                    <h2>콜드 브루</h2>
+                    <ul>
+                        <li>홈</li>
+                        <li>음료</li>
+                        <li>에스프레소</li>
+                        <li>화이트 초콜릿 모카</li>
+                    </ul>
+                </section>
+                <section className="contents">
+                    <article className="imageBox">
+                        <img src="/images/coffeeImg1.jpeg" alt="coffee" />
+                    </article>
+                    <aside className="details">
+                        <div className="coffee-title">
+                            <h2>화이트 초콜릿 모카</h2>
+                            <p>White Chocolate Mocha</p>
+                            <button className="heart">🤍</button>
+                        </div>
+                        <div className="description">
+                            달콤하고 부드러운 화이트 초콜릿 시럽과 에스프레소를 스팀 밀크와 섞어 휘핑크림으로 마무
+                            리한 음료를 달콤함과 강렬한 에스프레소가 부드럽게 어우러진 커피
+                        </div>
+                        <div className="product-info">
+                            <div className="product-head">
+                                <h3>제품 영양 정보</h3>
+                                <h3>Tall(톨) / 355ml(12 fl oz)</h3>
                             </div>
-                            <div className="description">
-                                달콤하고 부드러운 화이트 초콜릿 시럽과 에스프레소를 스팀 밀크와 섞어 휘핑크림으로 마무
-                                리한 음료를 달콤함과 강렬한 에스프레소가 부드럽게 어우러진 커피
+                            <div className="product-content">
+                                <ul>
+                                    <li>
+                                        <dl>
+                                            <dt>1회 제공량 (kcal)</dt>
+                                            <dd>345</dd>
+                                        </dl>
+                                    </li>
+                                    <li>
+                                        <dl>
+                                            <dt>포화지방 (g)</dt>
+                                            <dd>11</dd>
+                                        </dl>
+                                    </li>
+                                    <li>
+                                        <dl>
+                                            <dt>단백질 (g)</dt>
+                                            <dd>11</dd>
+                                        </dl>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li>
+                                        <dl>
+                                            <dt>나트륨 (mg)</dt>
+                                            <dd>150</dd>
+                                        </dl>
+                                    </li>
+                                    <li>
+                                        <dl>
+                                            <dt>당류 (g)</dt>
+                                            <dd>35</dd>
+                                        </dl>
+                                    </li>
+                                    <li>
+                                        <dl>
+                                            <dt>카페인 (mg)</dt>
+                                            <dd>75</dd>
+                                        </dl>
+                                    </li>
+                                </ul>
                             </div>
-                            <div className="product-info">
-                                <div className="product-head">
-                                    <h3>제품 영양 정보</h3>
-                                    <h3>Tall(톨) / 355ml(12 fl oz)</h3>
-                                </div>
-                                <div className="product-content">
-                                    <ul>
-                                        <li>
-                                            <dl>
-                                                <dt>1회 제공량 (kcal)</dt>
-                                                <dd>345</dd>
-                                            </dl>
-                                        </li>
-                                        <li>
-                                            <dl>
-                                                <dt>포화지방 (g)</dt>
-                                                <dd>11</dd>
-                                            </dl>
-                                        </li>
-                                        <li>
-                                            <dl>
-                                                <dt>단백질 (g)</dt>
-                                                <dd>11</dd>
-                                            </dl>
-                                        </li>
-                                    </ul>
-                                    <ul>
-                                        <li>
-                                            <dl>
-                                                <dt>나트륨 (mg)</dt>
-                                                <dd>150</dd>
-                                            </dl>
-                                        </li>
-                                        <li>
-                                            <dl>
-                                                <dt>당류 (g)</dt>
-                                                <dd>35</dd>
-                                            </dl>
-                                        </li>
-                                        <li>
-                                            <dl>
-                                                <dt>카페인 (mg)</dt>
-                                                <dd>75</dd>
-                                            </dl>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="allergy">알레르기 유발 요인 : 우유</div>
+                            <div className="allergy">알레르기 유발 요인 : 우유</div>
+                        </div>
+                        <div className="review">
+                            <span className="review-title">리뷰</span>
+                            <div className="review-box">
+                                <p><span className="userId">coffeelovers</span>너무 맛있어요!</p>
+                                <p><span className="userId">choco</span>오늘도 화이트 초콜릿모카 먹으러 갑시다</p>
+                                <p><span className="userId">legend_dev</span>진짜 화이트 초콜릿 모카는 전설이다.진짜 화이트 초콜릿 모카는 전설이다.진짜 화이트 초콜릿 모카는 전설이다.</p>
                             </div>
-                            <div className="review">
-                                <span className="review-title">리뷰</span>
-                                <div className="review-box">
-                                    <p><span className="userId">coffeelovers</span>너무 맛있어요!</p>
-                                    <p><span className="userId">choco</span>오늘도 화이트 초콜릿모카 먹으러 갑시다</p>
-                                    <p><span className="userId">legend_dev</span>진짜 화이트 초콜릿 모카는 전설이다.진짜 화이트 초콜릿 모카는 전설이다.진짜 화이트 초콜릿 모카는 전설이다.</p>
-                                </div>
-                                <section className="reivewInput-wrapper">
-                                    <input className="reviewInput" type="text" placeholder="리뷰를 입력해주세요!" />
-                                </section>
-                            </div>
-                        </aside>
-                    </section>
-                </main>
+                            <section className="reivewInput-wrapper">
+                                <input className="reviewInput" type="text" placeholder="리뷰를 입력해주세요!" />
+                            </section>
+                        </div>
+                    </aside>
+                </section>
             </div>
-            <footer>
+            <footer class="footer">
                 <ul>
                     COMPANY
                     <li>한눈에 보기</li>
--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,115 +1,113 @@
 import React from 'react';
-import './List.css'
-import '../../styles/common.css'
+import './List.scss'
+import styles from '../../styles/common.module.scss'
 
 function List() {
     return (
         <>
-        <div className="container">
-        <header>
-            <h1 className="logo">WeBucks</h1>
-            <nav>
-                <ul className="menu">
-                    <li>COFFEE</li>
-                    <li>MENU</li>
-                    <li>STORE</li>
-                    <li>WHAT'S NEW</li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <section className="section1">
-                <article className="title-box">
-                    <h2>콜드 브루 커피</h2>
-                    <span>☕️</span>
-                    <p>디카페인 에스포레소 샷 추가 가능 (일부 음료 제외)</p>
-                </article>
-                <article className="coffee-contents">
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee1" src="/images/coffeeImg1.jpeg" />
+            <div className={styles.container}>
+                <header className={styles.header}>
+                    <h1 className={styles.logo}>WeBucks</h1>
+                    <nav>
+                        <ul className={styles.menu}>
+                            <li>COFFEE</li>
+                            <li>MENU</li>
+                            <li>STORE</li>
+                            <li>WHAT'S NEW</li>
+                        </ul>
+                    </nav>
+                </header>
+                <section className="section1">
+                    <article className="title-box">
+                        <h2>콜드 브루 커피</h2>
+                        <span>☕️</span>
+                        <p>디카페인 에스포레소 샷 추가 가능 (일부 음료 제외)</p>
+                    </article>
+                    <article className="coffee-contents">
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee1" src="/images/coffeeImg1.jpeg" />
+                            </div>
+                            <p className="coffee-name">나이트로 바닐라 크림 ♡</p>
                         </div>
-                        <p className="coffee-name">나이트로 바닐라 크림 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee"src="/images/coffeeImg2.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg2.jpeg" />
+                            </div>
+                            <p className="coffee-name">돌체 콜드 브루 ♡</p>
                         </div>
-                        <p className="coffee-name">돌체 콜드 브루 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg3.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg3.jpeg" />
+                            </div>
+                            <p className="coffee-name">바닐라 크림 콜드 브루 ♡</p>
                         </div>
-                        <p className="coffee-name">바닐라 크림 콜드 브루 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg4.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg4.jpeg" />
+                            </div>
+                            <p className="coffee-name">벨벳 다크 모카 나이트로 ♡</p>
                         </div>
-                        <p className="coffee-name">벨벳 다크 모카 나이트로 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee5" src="/images/coffeeImg5.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee5" src="/images/coffeeImg5.jpeg" />
+                            </div>
+                            <p className="coffee-name">시그니처 더 블랙 콜드 브루 ♡</p>
                         </div>
-                        <p className="coffee-name">시그니처 더 블랙 콜드 브루 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg6.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg6.jpeg" />
+                            </div>
+                            <p className="coffee-name">제주 비자림 콜드 브루 ♡</p>
                         </div>
-                        <p className="coffee-name">제주 비자림 콜드 브루 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg7.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg7.jpeg" />
+                            </div>
+                            <p className="coffee-name">콜드 브루 ♡</p>
                         </div>
-                        <p className="coffee-name">콜드 브루 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg8.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg8.jpeg" />
+                            </div>
+                            <p className="coffee-name">콜드 브루 몰트 ♡</p>
                         </div>
-                        <p className="coffee-name">콜드 브루 몰트 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg9.jpeg"  />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg9.jpeg" />
+                            </div>
+                            <p className="coffee-name">콜드 브루 오트 라떼 ♡</p>
                         </div>
-                        <p className="coffee-name">콜드 브루 오트 라떼 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/coffeeImg10.jpeg" />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/coffeeImg10.jpeg" />
+                            </div>
+                            <p className="coffee-name">나이트로 콜드 브루 ♡</p>
                         </div>
-                        <p className="coffee-name">나이트로 콜드 브루 ♡</p>
+                    </article>
+                </section>
+                <section className="section1">
+                    <div className="title-box">
+                        <h2>브루드 커피</h2>
+                        <span>☕️</span>
+                        <p>디카페인 에스포레소 샷 추가 가능 (일부 음료 제외)</p>
                     </div>
-                </article>
-            </section>
-            <section className="section1">
-                <div className="title-box">
-                    <h2>브루드 커피</h2>
-                    <span>☕️</span>
-                    <p>디카페인 에스포레소 샷 추가 가능 (일부 음료 제외)</p>
-                </div>
-                <div className="coffee-contents">
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee1" src="/images/IcedCoffee1.jpeg" />
+                    <div className="coffee-contents">
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee1" src="/images/IcedCoffee1.jpeg" />
+                            </div>
+                            <p className="coffee-name">아이스 커피 ♡</p>
                         </div>
-                        <p className="coffee-name">아이스 커피 ♡</p>
-                    </div>
-                    <div className="coffee-item">
-                        <div className="coffee-imgbox">
-                            <img alt="coffee" src="/images/IcedCoffee2.jpeg"  />
+                        <div className="coffee-item">
+                            <div className="coffee-imgbox">
+                                <img alt="coffee" src="/images/IcedCoffee2.jpeg" />
+                            </div>
+                            <p className="coffee-name">오늘의 커피 ♡</p>
                         </div>
-                        <p className="coffee-name">오늘의 커피 ♡</p>
                     </div>
-                </div>
-            </section>
-        </main>
-    </div>   
+                </section>
+            </div>
         </>
     );
 }
